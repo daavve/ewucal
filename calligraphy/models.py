c774c59f81fb57abe7114022b9dbacd7192361ca
David McInnis <davidm@eagles.ewu.edu>
2016-01-27 21:10:44 -0800

    Added basic object model.

--- calligraphy/models.py
+++ calligraphy/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    book_id = models.IntegerField(primary_key=True)
+    book_title = models.CharField(max_length=16)
+    book_author = models.CharField(max_length=16)
+    book_transcriber = models.CharField(max_length=16)
+
+
+class Page(models.Model):
+    page_number = models.IntegerField()
+    book_parent = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+
+class Character(models.Model):
+    page_parent = models.ForeignKey(Page, on_delete=models.CASCADE)
+    char_mark = models.CharField(max_length=1)
+    x1 = models.IntegerField()
+    y1 = models.IntegerField()
+    x2 = models.IntegerField()
+    y2 = models.IntegerField()
+
+
+
