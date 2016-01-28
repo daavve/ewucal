c701d521dd4a614da43124953f31047a71416a04
David McInnis <davidm@eagles.ewu.edu>
2016-01-27 22:16:51 -0800

    modified admin to make books adable / editable by admin interface

--- calligraphy/admin.py
+++ calligraphy/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Book, Page, Character
+
+admin.site.register(Book)
+admin.site.register(Page)
+admin.site.register(Character)
