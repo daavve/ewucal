4fc81a4194b75766c8cd05d67334878f8139b889
David McInnis <davidm@eagles.ewu.edu>
2016-01-27 21:18:02 -0800

    Added notes field to the Database

--- calligraphy/models.py
+++ calligraphy/models.py
@@ -5,21 +5,24 @@ class Book(models.Model):
     book_id = models.IntegerField(primary_key=True)
     book_title = models.CharField(max_length=16)
     book_author = models.CharField(max_length=16)
-    book_transcriber = models.CharField(max_length=16)
+    book_transcriber = models.CharField(max_length=16,blank=True)
+    book_notes = models.CharField(max_length=128, blank=True)
 
 
 class Page(models.Model):
     page_number = models.IntegerField()
     book_parent = models.ForeignKey(Book, on_delete=models.CASCADE)
+    book_notes = models.CharField(max_length=128, blank=True)
 
 
 class Character(models.Model):
     page_parent = models.ForeignKey(Page, on_delete=models.CASCADE)
-    char_mark = models.CharField(max_length=1)
+    char_mark = models.CharField(max_length=1, blank=True)
     x1 = models.IntegerField()
     y1 = models.IntegerField()
     x2 = models.IntegerField()
     y2 = models.IntegerField()
+    char_notes = models.CharField(max_length=128, blank=True)
 
 
 
