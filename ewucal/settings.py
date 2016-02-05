7e2d412f6e160495e1a80a8f6255af777d7897ba
David McInnis <davidm@eagles.ewu.edu>
2016-02-04 22:53:42 -0800

    added setting for photologue

--- ewucal/settings.py
+++ ewucal/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'photologue',
+    'sortedm2m'  # Required for photologue
 ]
 
 MIDDLEWARE_CLASSES = [
