6c8a6a99fb4a27d3e9a6387e991fdc2f82e0b3c0
David McInnis <davidm@eagles.ewu.edu>
2016-02-05 16:10:53 -0800

    Update before working on ORM some more.

--- calligraphy/views.py
+++ calligraphy/views.py
@@ -1,4 +1,10 @@
+#
+# Logic of application goes here:  Receive Request -> Process -> Serve Response
+#
+###################################################
+
 from django.http import HttpResponse
+
 
 def index(request):
     return HttpResponse("Calligraphy App.")
