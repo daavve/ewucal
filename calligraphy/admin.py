7ae49e4aeb40d5bec903b1cc2ce543709aa731ed
David McInnis <davidm@eagles.ewu.edu>
2016-02-10 10:24:55 -0800

    improved clarity of admin interface somewhat.  Stil not at all intuative though....

--- calligraphy/admin.py
+++ calligraphy/admin.py
@@ -2,7 +2,23 @@ from django.contrib import admin
 
 from . import models
 
-admin.site.register(models.Collection)
-admin.site.register(models.Book)
-admin.site.register(models.Page)
-admin.site.register(models.Character)
+
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ('collection_name', 'id')
+
+
+class CollectionBook(admin.ModelAdmin):
+    list_display = ('book_title', 'book_author')
+
+
+class CollectionPage(admin.ModelAdmin):
+    list_display = ('page_number', 'page_image')
+
+
+class CollectionCharacter(admin.ModelAdmin):
+    list_display = ('char_mark', 'x1', 'y1', 'x2', 'y2', 'char_image')
+
+admin.site.register(models.Collection, CollectionAdmin)
+admin.site.register(models.Book, CollectionBook)
+admin.site.register(models.Page, CollectionPage)
+admin.site.register(models.Character, CollectionCharacter)
