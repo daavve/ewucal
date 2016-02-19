544c2d2fdb70db531be6f5a61b9518a36139443a
David McInnis <davidm@eagles.ewu.edu>
2016-02-18 22:18:18 -0800

    Think we have a minimal model now.

--- calligraphy/models.py
+++ calligraphy/models.py
@@ -7,27 +7,25 @@ fs = FileSystemStorage(location='images')
 class Author(models.Model):
     author_name = models.CharField(max_length=16)
     author_dynesty = models.CharField(max_length=16, blank=True)
-    author_notes = models.TextField(blank=True)
 
 
 class Work(models.Model):
-    work_title = models.CharField(max_length=16,blank=True)
+    work_id = models.IntegerField(primary_key=True)
     work_author = models.ForeignKey(Author)
-    work_transcriber = models.CharField(max_length=16,blank=True)
+    work_transcript = models.TextField(blank=True)
 
 
 class Page(models.Model):
     page_number = models.IntegerField()
-    page_notes = models.TextField(blank=True)
     page_image = models.ImageField(blank=True, storage=fs)
 
 
 class Character(models.Model):
     parent_page = models.ForeignKey(Page)
+    parent_author = models.ForeignKey(Author)
     char_mark = models.CharField(max_length=4, blank=True)
     x1 = models.IntegerField(blank=True)
     y1 = models.IntegerField(blank=True)
     x2 = models.IntegerField(blank=True)
     y2 = models.IntegerField(blank=True)
-    char_notes = models.TextField(blank=True)
     char_image = models.ImageField(blank=True, storage=fs)
