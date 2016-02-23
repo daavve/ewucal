85be2e27c23ddf71978fad55a71e8210286d0d56
David McInnis <davidm@eagles.ewu.edu>
2016-02-23 14:12:28 -0800

    fix problem I think

--- calligraphy/migrations/0001_initial.py
+++ calligraphy/migrations/0001_initial.py
@@ -50,7 +50,7 @@ def read_cworks(apps) -> None:
                         fileimg = fileimg.split('.')[0] + ".png"
                         if not os.path.isfile(fileimg):
                             raise Exception('Cannot find required image file', fileimg.split('.')[0])
-                img = open(fileimg, mode='r')
+                img = open(fileimg, mode='rb')
                 imgl = img.read()
                 img.close()
                 d_page = Page(parent_work=d_work, page_image=imgl)
