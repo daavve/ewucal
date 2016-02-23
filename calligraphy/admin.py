cd9e7149f0ae4d5c064574c1ce8e6c38eec8f5f5
David McInnis <davidm@eagles.ewu.edu>
2016-02-22 17:13:27 -0800

    commented out admin models to get migration script to work

--- calligraphy/admin.py
+++ calligraphy/admin.py
@@ -1,24 +1,24 @@
-from django.contrib import admin
+#from django.contrib import admin
 
-from . import models
-
-
-class CollectionAdmin(admin.ModelAdmin):
-    list_display = ('collection_name', 'id')
+#from . import models
 
 
-class CollectionBook(admin.ModelAdmin):
-    list_display = ('book_title', 'book_author')
+#class CollectionAdmin(admin.ModelAdmin):
+#    list_display = ('collection_name', 'id')
 
 
-class CollectionPage(admin.ModelAdmin):
-    list_display = ('page_number', 'page_image')
+#class CollectionBook(admin.ModelAdmin):
+#    list_display = ('book_title', 'book_author')
 
 
-class CollectionCharacter(admin.ModelAdmin):
-    list_display = ('char_mark', 'x1', 'y1', 'x2', 'y2', 'char_image')
+#class CollectionPage(admin.ModelAdmin):
+#    list_display = ('page_number', 'page_image')
 
-admin.site.register(models.Collection, CollectionAdmin)
-admin.site.register(models.Book, CollectionBook)
-admin.site.register(models.Page, CollectionPage)
-admin.site.register(models.Character, CollectionCharacter)
+
+#class CollectionCharacter(admin.ModelAdmin):
+#    list_display = ('char_mark', 'x1', 'y1', 'x2', 'y2', 'char_image')
+
+#admin.site.register(models.Collection, CollectionAdmin)
+#admin.site.register(models.Book, CollectionBook)
+#admin.site.register(models.Page, CollectionPage)
+#admin.site.register(models.Character, CollectionCharacter)
