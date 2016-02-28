13da7f1d2f22af1ab7fb895c7f434bb8b6ccc0a0
David McInnis <davidm@eagles.ewu.edu>
2016-02-27 20:56:31 -0800

    fixed typo

--- ewucal/settings.py
+++ ewucal/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'calligraphy.apps.calligraphy',
+    'calligraphy.apps.Calligraphy',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
