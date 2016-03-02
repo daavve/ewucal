b1f27294d822e117f3b5f316865c98f5cf941fc8
David McInnis <davidm@eagles.ewu.edu>
2016-03-01 20:56:37 -0800

    Added note comment on top about media layout.

--- calligraphy/urls.py
+++ calligraphy/urls.py
@@ -1,3 +1,15 @@
+#
+# ├── chars
+# │   ├── 06100004
+# │   │   ├── 00000009(266,179,402,296).jpg
+# ├── pages
+# │   ├── 06100004-00000009.(png,jpg,tif)
+# ├── scanned
+# │   └── 1
+# │       ├── 10i.png
+# │       ├── 10t.png
+#
+############################################
 from django.conf.urls import url
 from . import views
 
