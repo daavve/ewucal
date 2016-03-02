3ef67f731b6d0c69b11c668dcc687bfc6b32b89d
David McInnis <davidm@eagles.ewu.edu>
2016-03-01 22:09:14 -0800

    refered media url to calligraphy app.  For now it makes the implementation easier.

--- ewucal/urls.py
+++ ewucal/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^cal/', include('calligraphy.urls')),
+    url(r'^media/', include('calligraphy.urls')),
     url(r'^admin/', admin.site.urls),
 ]
