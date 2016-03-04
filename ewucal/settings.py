ce7a754c9890fa01f8b1abb6ab44834da49c592c
David McInnis <davidm@eagles.ewu.edu>
2016-03-03 21:58:38 -0800

    Fixed problem where we can't find html files.

--- ewucal/settings.py
+++ ewucal/settings.py
@@ -56,7 +56,8 @@ ROOT_URLCONF = 'ewucal.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'ewucal', 'calligraphy', 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'calligraphy', 'templates'),
+                 os.path.join(BASE_DIR, 'ewucal', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
