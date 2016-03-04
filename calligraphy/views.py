cb7172fbe0f4ae69882db9523defa1f72150eeee
David McInnis <davidm@eagles.ewu.edu>
2016-03-03 20:50:20 -0800

    Got author list to display on main page.

--- calligraphy/views.py
+++ calligraphy/views.py
@@ -4,12 +4,20 @@
 ###################################################
 
 from django.http import HttpResponse
+from django.template import Context, loader
+from .models import Author
+
 
 def base(request):
-    return HttpResponse("Base of the website")
+    authors = Author.objects.all()
+    tmplt = loader.get_template('calligraphy/authors.html')
+    cntxt = Context({'authors': authors})
+    return HttpResponse(tmplt.render(cntxt))
+
 
 def page_i(request):
     return HttpResponse("Image Request")
+
 
 def char_i(request):
     return HttpResponse("Character Request")
