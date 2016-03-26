d3fa05565a37147f80b1ae8f835efd3ddfd2b2c8
David McInnis <davidm@eagles.ewu.edu>
2016-03-25 20:54:22 -0700

    added easy_thumbnails to installed app list.

--- ewucal/settings.py
+++ ewucal/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'jquery_ui'
+    'jquery_ui',
+    'easy_thumbnails'
 ]
 
 MIDDLEWARE_CLASSES = [
