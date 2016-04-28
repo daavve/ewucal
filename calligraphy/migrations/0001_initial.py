a63a2c911cb9783f2998d6731f3b362d03373dce
David McInnis <davidm@eagles.ewu.edu>
2016-04-28 12:13:30 -0700

    fixed inconsistancy between names in object model and table names.

--- calligraphy/migrations/0001_initial.py
+++ calligraphy/migrations/0001_initial.py
@@ -91,7 +91,7 @@ class Migration(migrations.Migration):
                 ('x2', models.IntegerField(blank=True)),
                 ('y2', models.IntegerField(blank=True)),
                 ('char_image', models.ImageField(blank=True, storage=django.core.files.storage.FileSystemStorage(), upload_to='')),
-                ('char_author', models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, to='calligraphy.Author')),
+                ('parent_author', models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, to='calligraphy.Author')),
                 ('parent_work_name', models.CharField(blank=True, max_length=64)),
                 ('author_name', models.CharField(blank=True, max_length=64)),
             ],
