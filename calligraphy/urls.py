ec2175686eb25ea57580dc54a370b1a74579760d
David McInnis <davidm@eagles.ewu.edu>
2016-09-28 21:44:00 -0700

    updates

--- calligraphy/urls.py
+++ calligraphy/urls.py
@@ -19,8 +19,8 @@ urlpatterns = [
     url(r'^work/(?P<work_id>[0-9]+)$',  views.pages_in_work),
     url(r'^page/(?P<page_id>[0-9]+)$',  views.individual_page),
     url(r'^char/(?P<char_id>[0-9]+)$',  views.individual_char),
-    url(r'^ajax/get_page'),             views.get_page),
-    url(r'^ajax/get_page_chars'),       views.get_page_chars),
-    url(r'^ajax/get_char_relatives'),   views.get_char_relatives),
+    url(r'^ajax/get_page',             views.get_page),
+    url(r'^ajax/get_page_chars',       views.get_page_chars),
+    url(r'^ajax/get_char_relatives',   views.get_char_relatives),
     url(r'^$', views.auth_list)
 ]
