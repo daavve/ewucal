0c4e709ad4c82ae61749bd4546b8d496a448529b
David McInnis <davidm@eagles.ewu.edu>
2016-10-27 20:22:25 -0700

    basic functionality again

--- static/js/page.js
+++ static/js/page.js
@@ -180,53 +180,6 @@ function iWindow (iImg) {
 ////////////////////////////////////////////////////////////////////////////////////////
 
 
-function doFailThing(jqXHR, textStatus, url){
-  console.log("---- failure during request to: " + url + " ---------------");
-  console.log("Request failed: " + textStatus);
-};
-
-function addRelatives(thisChar){
-  var url = "/ajax/get_char_relatives";
-  $.ajax({
-    url: url,
-    dataType: "json",
-    method: "POST",
-    data: {charId: thisChar.pk}
-  }).done(function(relativesJSON){
-    relatives = JSON.parse(relativesJSON);
-
-    console.log("---------- Got Character Relatives ---------------");
-    console.log("thisChar: " + thisChar.pk);
-    console.log(relatives);
-
-    if(!charRelativesMap[thisChar.pk])
-      charRelativesMap[thisChar.pk] = [];
-
-    relatives.forEach(function(thisRelative){
-      // TODO: Implement this check if this relative is already in our map before
-      //       inserting it
-      // 9/25/2016
-      // - Michael Peterson
-
-      charRelativesMap[thisChar.pk].push(thisRelative);
-    }, this);
-
-    console.log("---------- Relatives map ---------------");
-    console.log(charRelativesMap);
-  }).fail(function(jqXHR, textStatus){
-    doFailThing(jqXHR, textStatus, url);
-  });;
-}
-
-// This function should render the bounding box around the characters on the
-// as well as wire up any event handlers they need etc.
-function buildCharInPage(thisChar){
-  // TODO: Implement this function
-  // 9/25/2016
-  // - Michael Peterson
-
-}
-
 function getPage(pageId){ // get the page object from the server  DOES NOT HANDLE ERRORS!
     return $.getJSON("/ajax/get_page", {"pageId" : pageId})
 }
