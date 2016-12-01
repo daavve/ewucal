10f3168e6240902f730f6355ddcd1fed815798a8
David McInnis <davidm@eagles.ewu.edu>
2016-12-01 14:56:25 -0800

    updated to include accoounts

--- ewucal/urls.py
+++ ewucal/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url, include
-from django.contrib import admin
 
-urlpatterns = []
-
-urlpatterns.append(url(r'^admin/', admin.site.urls))
-urlpatterns.append(url(r'^', include('calligraphy.urls')))
+urlpatterns = [
+    url(r'^accounts/', include('registration.backends.default.urls')),
+    url(r'^', include('calligraphy.urls'))
+]
