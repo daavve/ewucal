6bcc95778471ebcb3e57f0167d84f645a126e182
David McInnis <davidm@eagles.ewu.edu>
2017-01-26 18:31:36 -0800

    added function to grab csrftoken before every put request

---
+++ static/js/libs/get_csrf_token.js
@@ -0,0 +1,31 @@
+// From Django:  https://docs.djangoproject.com/en/1.10/ref/csrf/#ajax
+//
+// Grabs and returns csrf validation token for security
+function get_csrf_token() {
+    let name = 'csrftoken'
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", get_csrf_token());
+        }
+    }
+});
