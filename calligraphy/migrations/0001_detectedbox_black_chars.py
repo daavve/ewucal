8193cf684a7f665780d423e6479981c3ad6c6ba1
David McInnis <deedbeaf>
2017-09-07 14:03:05 -0700

    fixed dependency that doesnt exist

--- calligraphy/migrations/0001_detectedbox_black_chars.py
+++ calligraphy/migrations/0001_detectedbox_black_chars.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('calligraphy', 'tmp'),
+        ('calligraphy', '0108_auto_20170906_0336'),
     ]
 
     operations = [
